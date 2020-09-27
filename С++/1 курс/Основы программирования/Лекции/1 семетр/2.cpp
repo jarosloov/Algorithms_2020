a3173cb3e5fcd2305c52094b64517caaf6234781
Yaroslav <jarosloov@yandex.ru>
2020-09-27 07:25:59 +0500

    Update 2.cpp

--- С++/1 курс/Основы программирования/Лекции/1 семетр/2.cpp
+++ С++/1 курс/Основы программирования/Лекции/1 семетр/2.cpp
@@ -4,24 +4,24 @@ using namespace std;
  
 int main() {
  
-	double v,a,t,x,y,n,p,z;
+	double speed, corner, t, x, y, n, p, z;
  
 	const double G = 9.80665;
-	cin >>v>>a>>t;
-        a = (a*3.1415)/180;
-        cout<<"Получаем координаты:"<<endl;
+	cin >>speed>>corner>>t;
+        corner = (corner*3.1415)/180;
+        cout << "Получаем координаты:" << endl;
         z=t/45;
         p=t;
         t=z;
  
         while(t<=p){
-        x=v*t*cos(a);
-        y=(v*t*sin(a))-((G*pow(t, 2))/2);	
+        x=speed*t*cos(corner);
+        y=(speed*t*sin(corner))-((G*pow(t, 2))/2);	
 	if(y>0){
        cout<<" X="<<x<<" "<<" Y="<<y<<"\n";
         }
         else {
-         x=pow(v, 2)*sin(2*a)/G;
+         x = pow(speed, 2)*sin(2*corner)/G;
          cout<<" X="<<x<<" "<<" Y=0"<<"\n";	
         }
         t=t+z;
