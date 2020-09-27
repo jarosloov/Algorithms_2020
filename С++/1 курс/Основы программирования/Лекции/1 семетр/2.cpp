db4661c5859eecfeb5e1952d6e24fd002fc8172c
Yaroslav <jarosloov@yandex.ru>
2020-09-27 07:57:26 +0500

    обновка

--- С++/1 курс/Основы программирования/Лекции/1 семетр/2.cpp
+++ С++/1 курс/Основы программирования/Лекции/1 семетр/2.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
- 
-int main() {
- 
-	double speed, corner, t, x, y, n, p, z;
- 
-	const double G = 9.80665;
-	cin >>speed>>corner>>t;
-        corner = (corner*3.1415)/180;
+
+void coordinates(double totalTime, double clone_totalTime, double speed, double corner)
+{
+        const double G = 9.80665;       // постоянная 
+        double x = 0;
+        double y = 0;
+        double zero_time = 0;
         cout << "Получаем координаты:" << endl;
-        z=t/45;
-        p=t;
-        t=z;
- 
-        while(t<=p){
-        x=speed*t*cos(corner);
-        y=(speed*t*sin(corner))-((G*pow(t, 2))/2);	
-	if(y>0){
-       cout<<" X="<<x<<" "<<" Y="<<y<<"\n";
-        }
-        else {
-         x = pow(speed, 2)*sin(2*corner)/G;
-         cout<<" X="<<x<<" "<<" Y=0"<<"\n";	
-        }
-        t=t+z;
+        while(zero_time <= clone_totalTime)
+        {
+                x = speed * zero_time * cos(corner);
+                y = (speed * zero_time * sin(corner)) - (( G * pow(zero_time, 2)) / 2);	
+	        if(y > 0)
+                {
+                        cout<<" X="<<x<<" "<<" Y="<<y<< endl;
+                }
+                        else {
+                                x = pow(speed, 2) * sin(2 * corner) / G;
+                                cout << " X=" << x << " " << " Y=0" << endl;	
+                        }
+                zero_time += totalTime;
       } 
+}
+
+
+int main() 
+{
+ 
+	double speed, corner, totalTime, n, clone_totalTime, zero_time;
+        int time;
+ 
+	cin >> speed;                           // вводим скорость
+        cin >> corner;                          // вводим угол под которм брошено тело
+        cin >> totalTime;                       // вводим время полёта
+        corner = (corner * 3.1415) / 180;       // переводим градусы в радианы, потому что sin и cos считают в радианах
+
+        clone_totalTime = totalTime;
+
+        cout << "Введите число, на сколько промежутков вы хотите разделить время" << endl;
+        cin >> time;
+        totalTime = totalTime / time;
+        coordinates(totalTime,  clone_totalTime,  speed, corner);
+
 	return 0;
 }
