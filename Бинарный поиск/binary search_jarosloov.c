9ef7c961ecdccc3310a444ec13cd448166ad6276
Yaroslav <60881838+jarosloov@users.noreply.github.com>
2020-10-14 23:27:47 +0500

    Update binary search_jarosloov.c

--- Бинарный поиск/binary search_jarosloov.c
+++ Бинарный поиск/binary search_jarosloov.c
@@ -32,28 +32,3 @@ int main()
     printf("%d\n", binarySearch(myList, 3, len));  
     return 0;
 }
-
-int binarySearch(int list[], int item, int len)
-{
-    int low = 0;
-    int high = len;
-    while (low <= high)
-    {
-        int mid = (low + high)/2; 
-        int guess = list[mid];
-
-        if (guess == item)
-        {
-            return mid;
-        }
-        else if (guess > item)
-        {
-            high = mid - 1;
-        }
-        else
-        {
-            low = mid + 1;
-        }
-    }
-    return -1; //number not found
-}
